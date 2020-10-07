7108d0695b9ff0df5a4eb28f0ade4995377afb65
Jon Muller <jon.muller@4mation.com.au>
2020-10-08 10:32:44 +1100

    Site meta

--- components/AppMasthead.vue
+++ components/AppMasthead.vue
@@ -8,7 +8,7 @@
 export default {
   data() {
     return {
-      tagline: "Headless WordPress on the JAMstack"
+      tagline: "nfk all the st"
     };
   }
 };
@@ -25,30 +25,31 @@ section.masthead {
   justify-content: center;
   align-items: center;
   padding: 7vw;
-  background: -moz-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(0, 0, 0, 0) 0%,
-      rgba(0, 0, 0, 0) 37%,
-      rgba(0, 0, 0, 0.65) 100%
-    ),
-    url("/mountains-masthead.jpg") no-repeat center center scroll; /* FF3.6-15 */
-  background: -webkit-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(0, 0, 0, 0) 0%,
-      rgba(0, 0, 0, 0) 37%,
-      rgba(0, 0, 0, 0.65) 100%
-    ),
-    url("/mountains-masthead.jpg") no-repeat center center scroll; /* Chrome10-25,Safari5.1-6 */
-  background: radial-gradient(
-      ellipse at center,
-      rgba(0, 0, 0, 0) 0%,
-      rgba(0, 0, 0, 0) 37%,
-      rgba(0, 0, 0, 0.65) 100%
-    ),
-    url("/mountains-masthead.jpg") no-repeat center center scroll; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  background-size: cover;
+  background-color: #333;
+  // background: -moz-radial-gradient(
+  //     center,
+  //     ellipse cover,
+  //     rgba(0, 0, 0, 0) 0%,
+  //     rgba(0, 0, 0, 0) 37%,
+  //     rgba(0, 0, 0, 0.65) 100%
+  //   ),
+  //   url("/mountains-masthead.jpg") no-repeat center center scroll; /* FF3.6-15 */
+  // background: -webkit-radial-gradient(
+  //     center,
+  //     ellipse cover,
+  //     rgba(0, 0, 0, 0) 0%,
+  //     rgba(0, 0, 0, 0) 37%,
+  //     rgba(0, 0, 0, 0.65) 100%
+  //   ),
+  //   url("/mountains-masthead.jpg") no-repeat center center scroll; /* Chrome10-25,Safari5.1-6 */
+  // background: radial-gradient(
+  //     ellipse at center,
+  //     rgba(0, 0, 0, 0) 0%,
+  //     rgba(0, 0, 0, 0) 37%,
+  //     rgba(0, 0, 0, 0.65) 100%
+  //   ),
+  //   url("/mountains-masthead.jpg") no-repeat center center scroll; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  // background-size: cover;
   color: #333;
   h1 {
     color: white;
