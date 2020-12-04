382c26b732ffbcc654b162ce3587267a7c04c761
Jon Muller <jon.muller@gmail.com>
2020-12-04 15:38:14 +1100

    Formmm

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,7 +4,7 @@
 			Contact me
 		</h1>
 		<p>Email me at <a href="mailto:jon.muller@gmail.com">jon.muller@gmail.com</a> or call me on <a href="tel:0466213376">0466&nbsp;213&nbsp;376</a>.</p>
-		<form name="contact" method="POST" data-netlify="true" @submit.prevent="handleSubmit">
+		<form action="/" name="contact" method="POST" data-netlify="true" @submit.prevent="handleSubmit">
 			<input type="hidden" name="form-name" value="contact" />
 			<label>Your Name: <input type="text" name="name"/></label>
 			<label>Your Email: <input type="email" name="email"/></label>
