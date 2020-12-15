50d4ecd980481166f444c95f124839b618290341
Jon Muller <jon.muller@gmail.com>
2020-12-16 06:19:23 +1100

    Performance tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -25,67 +25,10 @@
 			email me at <a href="mailto:jon.muller@gmail.com">jon.muller@gmail.com</a><br class="soft" />
 			or <nuxt-link to="/about/">read more about me</nuxt-link>.
 		</p>
-		<!-- <main>
-			<div class="post" v-for="post in sortedPosts" :key="post.id">
-				<h3>
-					<a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
-				</h3>
-				<small>{{ post.date | dateformat }}</small>
-				<div v-html="post.excerpt.rendered"></div>
-				<a :href="`blog/${post.slug}`" class="readmore slide">Read more ⟶</a>
-			</div>
-		</main>
-		<aside>
-			<h2 class="tags-title">Tags</h2>
-			<div class="tags-list">
-				<ul>
-					<li @click="updateTag(tag)" v-for="tag in tags" :key="tag.id" :class="[tag.id === selectedTag ? activeClass : '']">
-						<a>{{ tag.name }}</a>
-						<span v-if="tag.id === selectedTag">✕</span>
-					</li>
-				</ul>
-			</div>
-		</aside> -->
 	</div>
 </template>
 
-<script>
-export default {
-	components: {},
-	data() {
-		return {
-			selectedTag: null,
-			activeClass: 'active'
-		};
-	},
-	computed: {
-		posts() {
-			return this.$store.state.posts;
-		},
-		tags() {
-			return this.$store.state.tags;
-		},
-		sortedPosts() {
-			if (!this.selectedTag) return this.posts;
-			return this.posts.filter(el => el.tags.includes(this.selectedTag));
-		}
-	},
-	created() {
-		this.$store.dispatch('getPosts');
-	},
-	methods: {
-		updateTag(tag) {
-			if (!this.selectedTag) {
-				this.selectedTag = tag.id;
-			} else {
-				this.selectedTag = null;
-			}
-		}
-	}
-};
-</script>
-
-<style lang="scss" scroped>
+<style lang="scss">
 @import '@/assets/variables.scss';
 @import '@/assets/mixins.scss';
 @import '@/assets/include-media.scss';
--- store/index.js
+++ store/index.js
@@ -1,66 +1,66 @@
-const siteURL = "http://54.206.97.106"
+// const siteURL = "http://54.206.97.106"
 
-export const state = () => ({
-  posts: [],
-  tags: []
-})
+// export const state = () => ({
+//   posts: [],
+//   tags: []
+// })
 
-export const mutations = {
-  updatePosts: (state, posts) => {
-    state.posts = posts
-  },
-  updateTags: (state, tags) => {
-    state.tags = tags
-  }
-}
+// export const mutations = {
+//   updatePosts: (state, posts) => {
+//     state.posts = posts
+//   },
+//   updateTags: (state, tags) => {
+//     state.tags = tags
+//   }
+// }
 
-export const actions = {
-  async getPosts({ state, commit, dispatch }) {
-    if (state.posts.length) return
+// export const actions = {
+//   async getPosts({ state, commit, dispatch }) {
+//     if (state.posts.length) return
 
-    try {
-      let posts = await fetch(
-        `${siteURL}/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1&categories=2`
-      ).then(res => res.json())
+//     try {
+//       let posts = await fetch(
+//         `${siteURL}/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1&categories=2`
+//       ).then(res => res.json())
 
-      posts = posts
-        .filter(el => el.status === "publish")
-        .map(({ id, slug, title, excerpt, date, tags, content }) => ({
-          id,
-          slug,
-          title,
-          excerpt,
-          date,
-          tags,
-          content
-        }))
+//       posts = posts
+//         .filter(el => el.status === "publish")
+//         .map(({ id, slug, title, excerpt, date, tags, content }) => ({
+//           id,
+//           slug,
+//           title,
+//           excerpt,
+//           date,
+//           tags,
+//           content
+//         }))
 
-      commit("updatePosts", posts)
-    } catch (err) {
-      console.log(err)
-    }
-  },
-  async getTags({ state, commit }) {
-    if (state.tags.length) return
+//       commit("updatePosts", posts)
+//     } catch (err) {
+//       console.log(err)
+//     }
+//   },
+//   async getTags({ state, commit }) {
+//     if (state.tags.length) return
 
-    let allTags = state.posts.reduce((acc, item) => {
-      return acc.concat(item.tags)
-    }, [])
-    allTags = allTags.join()
+//     let allTags = state.posts.reduce((acc, item) => {
+//       return acc.concat(item.tags)
+//     }, [])
+//     allTags = allTags.join()
 
-    try {
-      let tags = await fetch(
-        `${siteURL}/wp-json/wp/v2/tags?page=1&per_page=40&include=${allTags}`
-      ).then(res => res.json())
+//     try {
+//       let tags = await fetch(
+//         `${siteURL}/wp-json/wp/v2/tags?page=1&per_page=40&include=${allTags}`
+//       ).then(res => res.json())
 
-      tags = tags.map(({ id, name }) => ({
-        id,
-        name
-      }))
+//       tags = tags.map(({ id, name }) => ({
+//         id,
+//         name
+//       }))
 
-      commit("updateTags", tags)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-}
+//       commit("updateTags", tags)
+//     } catch (err) {
+//       console.log(err)
+//     }
+//   }
+// }
